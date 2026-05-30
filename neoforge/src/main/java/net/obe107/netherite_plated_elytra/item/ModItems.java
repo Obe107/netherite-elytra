855c3f0a8edf19abc0eef04e0e22df0d5daf9b00
Obe107 <107483675+Obe107@users.noreply.github.com>
2026-05-30 07:54:46 +0200

    Release 0.0.4

--- neoforge/src/main/java/net/obe107/netherite_plated_elytra/item/ModItems.java
+++ neoforge/src/main/java/net/obe107/netherite_plated_elytra/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.obe107.netherite_plated_elytra.item;
 
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.sounds.SoundEvents;
@@ -11,7 +10,6 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.component.DamageResistant;
